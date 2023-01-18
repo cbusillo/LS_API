6bd004b39f56cc618922ed9087b66d54f7914143
Chris Busillo <info@shinycomputers.com>
2023-01-17 23:17:26 -0500

    clean up updatecustomerphone

--- updateCustomerPhone.py
+++ updateCustomerPhone.py
@@ -6,14 +6,14 @@ def runUpdateCustomerPhone():
     customersUpdated = 0
     for index, customer in enumerate(customers):
         if customer.Contact.Phones:
-            for indexNumber, eachNumber in enumerate(customer.Contact.Phones.ContactPhone):
-                if eachNumber.number != re.sub(r'[^0-9x]', '', eachNumber.number):
-                    eachNumber.number = re.sub(r'[^0-9x]', '', eachNumber.number)
+            for eachNumber in customer.Contact.Phones.ContactPhone:
+                cleanedNumber = re.sub(r'[^0-9x]', '', eachNumber.number)
+                if eachNumber.number != cleanedNumber:
+                    eachNumber.number = cleanedNumber
                     customer.isModified = True
                 if len(eachNumber.number) == 7:
                     eachNumber.number = '757' + eachNumber.number 
                     customer.isModified = True
-                #print(f'{index} {customer.contactID} {customer.Contact.Phones.ContactPhone[indexNumber].number} {customer.firstName}')
         if customer.isModified == True:
             customersUpdated += 1
             print(f"{customersUpdated}: Updating Customer #{index} out of {len(customers)}                         ", end='\r')
