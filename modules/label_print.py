2a43857eb53d99e995a0e6e70c20c7fab175641c
Chris Busillo <info@shinycomputers.com>
2023-01-18 19:35:32 -0500

    Inital working printing and gui

--- modules/label_print.py
+++ modules/label_print.py
@@ -8,5 +8,8 @@ def print_text(text: str):
     host = "192.168.1.240"
     port = 9100
     mysocket.connect((host, port))  # connecting to host
-    mysocket.send(b"^XA^A0N,50,50^FO50,50^FD" + text + b"^FS^XZ")  # using bytes
+    mysocket.send(b"^XA^A0N,50,50^FO0,50^FB450,4,,C,^FD" + bytes(text, "utf-8") + b"^FS^XZ")  # using bytes
     mysocket.close()  # closing connection
+
+
+print_text("Theatrical Builds\&1.18.2023")
