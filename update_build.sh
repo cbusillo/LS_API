543d17cd9fc9c28ab65f3758c92f631e1e37bc86
Chris Busillo <info@shinycomputers.com>
2023-01-19 13:06:30 -0500

    testing update updater code

--- update_build.sh
+++ update_build.sh
@@ -1,4 +1,14 @@
-git pull https://github.com/cbusillo/LS_API
+#!/bin/bash
+killall Python &> /dev/null
+if git diff --quiet
+then
+    echo "test"
+    exit
+    git pull https://github.com/cbusillo/LS_API
+    $(basename $0) && exit     
+fi
+exit
 brew install python-tk@3.11
 pip3.11 install -r requirements.txt
 #make binary on desktop
+./gui.py
