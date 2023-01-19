81283d13f922b63662c97497f8911e7e46ad3954
Chris Busillo <info@shinycomputers.com>
2023-01-19 13:11:27 -0500

    update updater script with arm/x86

--- update_build.sh
+++ update_build.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 killall Python &> /dev/null
-if git diff -- update_build.py --quiet
+if ! git diff -- update_build.py --quiet
 then
-    echo "test"
-    exit
+    echo "Updating files."
     git pull https://github.com/cbusillo/LS_API
+    echo "Restarting script."
     $(basename $0) && exit     
 fi
-exit
-brew install python-tk@3.11
+
+if [[ $(uname -m) == 'arm64' ]]; then
+    arch -arm64 brew upgrade python-tk@3.11
+else
+    brew upgrade python-tk@3.11
+fi
+
 pip3.11 install -r requirements.txt
 #make binary on desktop
 ./gui.py
