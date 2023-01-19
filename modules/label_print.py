90658742b048e8375ca3466d95fba25e57407c6d
Chris Busillo <info@shinycomputers.com>
2023-01-18 20:33:44 -0500

    removed debug code that would print on startup

--- modules/label_print.py
+++ modules/label_print.py
@@ -10,6 +10,3 @@ def print_text(text: str):
     mysocket.connect((host, port))  # connecting to host
     mysocket.send(b"^XA^A0N,50,50^FO0,50^FB450,4,,C,^FD" + bytes(text, "utf-8") + b"^FS^XZ")  # using bytes
     mysocket.close()  # closing connection
-
-
-print_text("Theatrical Builds\&1.18.2023")
