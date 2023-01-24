b1ead90ce6b3c2a16b6abf1ca0d2ab85c75c6067
Chris Busillo <info@shinycomputers.com>
2023-01-24 11:20:52 -0500

    new homebrew install

--- update_build.sh
+++ update_build.sh
@@ -13,7 +13,7 @@ fi
 which -s brew
 if [[ $? != 0 ]] ; then
     # Install Homebrew
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
     brew update
 fi
