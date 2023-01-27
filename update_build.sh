5e49a44d75f097ba7f666973d87f2d079038cc5f
Chris Busillo <info@shinycomputers.com>
2023-01-27 18:25:06 -0500

    update script forces x86

--- update_build.sh
+++ update_build.sh
@@ -18,15 +18,12 @@ fi
 which -s brew
 if [[ $? != 0 ]] ; then
     # Install Homebrew
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    arch -x86_64 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
     brew update
 fi
-if [[ $(uname -m) == 'arm64' ]]; then
-    arch -arm64 brew install python@3.11
-else
-    brew install python@3.11
-fi
+
+brew install python@3.11
 brew upgrade
 brew install pkg-config sdl2 sdl2_image sdl2_ttf sdl2_mixer
 pip3.11 install virtualenv
@@ -41,7 +38,7 @@ activate
 #pip3.11 install git+https://github.com/SciTools/cartopy.git test
 pip3.11 install -U -r requirements-manual.txt
 pip3.11 install -U -r requirements.txt
-
+brew install tesseract
 if [[ $(uname -m) == 'arm64' ]]; then
     pip3.11 install kivy --pre --no-deps --index-url  https://kivy.org/downloads/simple/
     #pip3.11 install "kivy[base]" --pre --extra-index-url https://kivy.org/downloads/simple/
