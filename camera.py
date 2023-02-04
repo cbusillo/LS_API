f1fe677207f094ab2e0fcc6146a86f3ff87b5aca
Chris Busillo <info@shinycomputers.com>
2023-02-03 22:02:33 -0500

    build camera interface,

--- camera.py
+++ camera.py
@@ -13,6 +13,7 @@ from kivy.graphics.texture import Texture
 from kivy.uix.button import Label, Button
 from kivy.uix.image import Image
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.slider import Slider
 import numpy as np
 from skimage.transform import rotate
 from deskew import determine_skew
@@ -20,7 +21,6 @@ from modules import load_config as config
 from classes import sickw_results
 from classes.google_mysql import Database
 from classes.api_serial import Serial
-import modules.connect_sickw as connect_sickw
 
 # from classes import google_sheets
 
@@ -70,19 +70,44 @@ class PhotoWindow(GridLayout):
         self.rotate_button = Button(text="Rotate", halign="center", size_hint=(0.1, 0.1))
         self.rotate_button.bind(on_press=self.rotate_button_fn)
         self.add_widget(self.rotate_button)
-        self.threshold_up_button = Button(text="Threshold up", halign="center", size_hint=(0.1, 0.1))
-        self.threshold_up_button.bind(on_press=partial(self.threshold_button_fn, 5))
-        self.add_widget(self.threshold_up_button)
-        self.threshold_down_button = Button(text="Threshold down", halign="center", size_hint=(0.1, 0.1))
-        self.threshold_down_button.bind(on_press=partial(self.threshold_button_fn, -5))
-        self.add_widget(self.threshold_down_button)
+
+        self.threshold_slider = Slider(min=0, max=255, value=180, size_hint=(1, 0.15))
+        self.threshold_slider.bind(value=self.threshold_change)
+        self.add_widget(self.threshold_slider)
+
+        self.threshold_grid = GridLayout()
+        self.threshold_grid.cols = 3
+        self.threshold_grid.size_hint_y = 0.1
+
+        self.threshold_down_button = Button(text="Threshold down", halign="center")
+        self.threshold_down_button.bind(on_press=partial(self.threshold_change, change=-5))
+        self.threshold_grid.add_widget(self.threshold_down_button)
+
+        self.threshold_label = Label(text=str(self.threshold_slider.value))
+        self.threshold_grid.add_widget(self.threshold_label)
+
+        self.threshold_up_button = Button(text="Threshold up", halign="center")
+        self.threshold_up_button.bind(on_press=partial(self.threshold_change, change=5))
+        self.threshold_grid.add_widget(self.threshold_up_button)
+
+        self.add_widget(self.threshold_grid)
+
+        self.image_grid = GridLayout()
+        self.image_grid.cols = 2
 
         self.scanned_image = Image()
         self.scanned_image.width = cv2.CAP_PROP_FRAME_WIDTH
         self.scanned_image.height = cv2.CAP_PROP_FRAME_HEIGHT
-        self.add_widget(self.scanned_image)
+        self.image_grid.add_widget(self.scanned_image)
 
-        self.status = Label()
+        self.threshed_image = Image()
+        self.threshed_image.width = cv2.CAP_PROP_FRAME_WIDTH
+        self.threshed_image.height = cv2.CAP_PROP_FRAME_HEIGHT
+        self.image_grid.add_widget(self.threshed_image)
+
+        self.add_widget(self.image_grid)
+
+        self.status = Label(size_hint=(0.8, 0.2))
         self.add_widget(self.status)
 
         self.capture = cv2.VideoCapture(config.CAM_PORT)
@@ -93,13 +118,13 @@ class PhotoWindow(GridLayout):
         self.fps_previous = 0
         self.fps_current = 0
         self.rotation = 1
-        self.theshold_value = 180
+        # self.theshold_value = 180
 
     def thresh_image(self, image):
         """take grayscale image and return Threshholded image"""
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         image = deskew(image)
-        _, image = cv2.threshold(image, self.theshold_value, 255, cv2.THRESH_BINARY)
+        _, image = cv2.threshold(image, self.threshold_slider.value, 255, cv2.THRESH_BINARY)
         # image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 15)
 
         return image
@@ -110,9 +135,10 @@ class PhotoWindow(GridLayout):
         else:
             self.rotation = -1
 
-    def threshold_button_fn(self, change, _):
-        if self.theshold_value + change < 255 and self.theshold_value + change > -1:
-            self.theshold_value += change
+    def threshold_change(self, _, value=None, change=None):
+        if change:
+            self.threshold_slider.value += change
+        self.threshold_label.text = str(int(self.threshold_slider.value))
 
     def update(self, _):
         """Handle clock updates"""
@@ -126,39 +152,43 @@ class PhotoWindow(GridLayout):
                 output_type=pytesseract.Output.DICT,
                 config="--psm 11",  # -c tessedit_char_whitelist=' 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'",
             )
-        display_lines = ""
-        miss = True
+        display_lines = "No succesful reads"
         for conf, word in zip(serial_image_data["conf"], serial_image_data["text"]):
-            if conf > 60 and len(word) >= 8 and re.sub(r"[^A-Z0-9]", "", word) == word:
+            if conf > 40 and len(word) >= 8 and re.sub(r"[^A-Z0-9]", "", word) == word:
                 blacklisted = False
                 for black in BLACKLIST:
                     if black in word:
                         blacklisted = True
-                # if word.upper().strip() == "GG7X2LZ6JF88":
+
                 if not any(d.serial_number == word for d in self.sickw_history) and not blacklisted:
                     sickw = sickw_results.SickwResults(word, sickw_results.APPLE_SERIAL_INFO)
-                    if sickw and sickw.status == "success":
-                        self.sickw_history.append(sickw)
-                output = f"Conf: {conf} {word} Total: {len(self.sickw_history)} {self.sickw_history[-1].name}"
-
-                display_lines += f" {output}\n"
-                miss = False
-
-        if miss is True:
-            print(".", end="")
+                    self.sickw_history.append(sickw)
+                if not blacklisted:
+                    output = f"Conf: {conf} {word} Total: {len(self.sickw_history)} "
+                    output += f"Matches: {sickw_results.SickwResults.search_list_for_serial(word, self.sickw_history)} "
+                    output += f"Sucessful: {sickw_results.SickwResults.success_count(self.sickw_history)}"
+                    display_lines += f" {output}\n"
+                    print(display_lines)
         self.status.text = display_lines
 
         self.fps_current = time.time()
         fps = 1 / (self.fps_current - self.fps_previous)
         self.fps_previous = self.fps_current
         cv2.putText(threshed, str(round(fps, 2)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 3)
+        cv2.putText(serial_image, str(round(fps, 2)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 3)
 
         # cv2.imshow("test", threshed)
+        buf1 = cv2.flip(serial_image, 0)
+        buf = buf1.tobytes()
+        # if self.scanned_image.texture is None:
+        self.scanned_image.texture = Texture.create(size=(config.CAM_WIDTH, config.CAM_HEIGHT), colorfmt="bgr")
+        self.scanned_image.texture.blit_buffer(buf, colorfmt="bgr", bufferfmt="ubyte")
+
         buf1 = cv2.flip(threshed, 0)
         buf = buf1.tobytes()
         # if self.scanned_image.texture is None:
-        self.scanned_image.texture = Texture.create(size=(config.CAM_WIDTH, config.CAM_HEIGHT), colorfmt="luminance")
-        self.scanned_image.texture.blit_buffer(buf, colorfmt="luminance", bufferfmt="ubyte")
+        self.threshed_image.texture = Texture.create(size=(config.CAM_WIDTH, config.CAM_HEIGHT), colorfmt="luminance")
+        self.threshed_image.texture.blit_buffer(buf, colorfmt="luminance", bufferfmt="ubyte")
 
 
 class SerialCamera(App):
@@ -167,7 +197,7 @@ class SerialCamera(App):
     def build(self):
         Window.left = 0  # 0
         Window.top = 0
-        Window.size = (config.CAM_WIDTH / 2, config.CAM_HEIGHT)
+        Window.size = (config.CAM_WIDTH / 2, config.CAM_HEIGHT * 0.7)
         return PhotoWindow()
 
 
