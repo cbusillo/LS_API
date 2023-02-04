268dd057b64564d1fb3320d02cc7cbdd1012c746
Chris Busillo <info@shinycomputers.com>
2023-02-04 14:35:05 -0500

    inital windows install

---
+++ install_client.bat
@@ -0,0 +1,29 @@
+set APPNAME="Shiny_API"
+set PYTHONVERSION=10
+set "PYTHONROOT=%LOCALAPPDATA%\Programs\Python"
+for /d %%d in (%PYTHONROOT%\Python3%REQVERSION%*) do (set "PYTHONVERSION=%%d" & goto break)
+:break
+set "PYTHON=%PYTHONVERSION%\python"
+
+
+tasklist | find /i "python3.exe" && taskkill /im "python3.exe" /F || echo process "python3.exe" not running
+
+%PYTHON% --version
+if %errorlevel% NEQ 0 (
+	winget install --silent  --accept-package-agreements --accept-source-agreements python3.%PYTHONVERSION%
+	echo "Restarting script."
+	%0
+	exit
+
+)
+%PYTHON% -m pip install virtualenv
+
+if not exist %APPNAME% (
+	%PYTHON% -m virtualenv %APPNAME%
+)
+
+%APPNAME%\scripts\activate.bat
+
+%PYTHON% -m pip install --upgrade pip
+%PYTHON% -m pip install --upgrade %APPNAME%
+
