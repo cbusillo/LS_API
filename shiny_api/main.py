a91fd6560ec357d85e12bee61fcc9c86d5729cab
Chris Busillo <info@shinycomputers.com>
2023-02-12 20:45:16 -0500

    case typo

--- shiny_api/main.py
+++ shiny_api/main.py
@@ -28,7 +28,7 @@ from kivy.core.window import Window  # pylint: disable=wrong-import-order
 from kivy.uix.textinput import TextInput  # pylint: disable=wrong-import-order
 
 MY_COMPUTER = "chris-mbp"
-SERVER = ["secureErase", MY_COMPUTER]
+SERVER = ["secureerase", MY_COMPUTER]
 
 if platform.node().lower() == MY_COMPUTER:
     config.DEBUG_CODE = True
