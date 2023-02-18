b807d619e85e4402e34c92b07c9e4f19b4204406
Chris Busillo <info@shinycomputers.com>
2023-02-18 12:28:50 -0500

    change from zpl to simple_zpl2 for smaller barcode

--- shiny_api/modules/label_print.py
+++ shiny_api/modules/label_print.py
@@ -1,79 +1,67 @@
 """Zebra printing module"""
 import datetime
-import socket
 import os
 from typing import List
-import zpl
+from simple_zpl2 import ZPLDocument, Code128_Barcode, NetworkPrinter
 from shiny_api.modules import load_config as config
 
 print(f"Importing {os.path.basename(__file__)}...")
-LABEL_SIZE = {"width": 36.75, "height": 21}
-LABEL_TEXT_SIZE = {"width": 4, "height": 4, "small_width": 2, "small_height": 2}
-LABEL_PADDING = 1
+LABEL_SIZE = {"width": 2, "height": 1.3}
+LABEL_TEXT_SIZE = {"width": 40, "height": 40, "small_width": 20, "small_height": 20}
+LABEL_PADDING = 10
 BARCODE_HEIGHT = 35
 
 
-def print_text(text: List[str] = None, barcode: str = None, text_bottom: str = "", quantity: int = 1, print_date: bool = True):
+def print_text(text: List[str] = None, barcode: str = None, text_bottom: str = None, quantity: int = 1, print_date: bool = True):
     """Open socket to printer and send text"""
     if not isinstance(text, list):
         text = [text]
 
+    if text_bottom is None:
+        text_bottom = ""
+    if barcode is None:
+        barcode = ""
+
+    label_width = int(203 * LABEL_SIZE["width"])
+    label_height = int(203 * LABEL_SIZE["height"])
+
     quantity = max(int(quantity), 1)
-    label = zpl.Label(width=LABEL_SIZE["width"], height=LABEL_SIZE["height"])
+    label = ZPLDocument()
+    label.add_zpl_raw("^BY2")
+    label.add_default_font(font_name=0, character_height=LABEL_TEXT_SIZE["height"], character_width=LABEL_TEXT_SIZE["width"])
     current_origin = LABEL_PADDING
     for index, line in enumerate(text):
-        label.origin(x=0, y=current_origin)
-        label.write_text(
-            line,
-            char_height=LABEL_TEXT_SIZE["width"],
-            char_width=LABEL_TEXT_SIZE["height"],
-            line_width=LABEL_SIZE["width"],
-            justification="C",
-        )
-        label.endorigin()
+        label.add_field_block(text_justification="C", width=label_width)
+        label.add_field_origin(x_pos=LABEL_PADDING, y_pos=current_origin, justification=2)
+        label.add_field_data(line)
         current_origin = (index + 1) * LABEL_TEXT_SIZE["height"] + LABEL_PADDING
 
     if print_date:
         today = datetime.date.today()
         formatted_date = f"{today.month}.{today.day}.{today.year}"
-        label.origin(x=0, y=current_origin)
-        label.write_text(
-            formatted_date,
-            char_height=LABEL_TEXT_SIZE["small_width"],
-            char_width=LABEL_TEXT_SIZE["small_height"],
-            line_width=LABEL_SIZE["width"],
-            justification="C",
+        label.add_default_font(
+            font_name=0, character_height=LABEL_TEXT_SIZE["small_height"], character_width=LABEL_TEXT_SIZE["small_width"]
         )
-        label.endorigin()
+        label.add_field_block(text_justification="C", width=label_width)
+        label.add_field_origin(x_pos=LABEL_PADDING, y_pos=current_origin, justification=2)
+        label.add_field_data(formatted_date)
         current_origin = current_origin + (LABEL_TEXT_SIZE["small_height"])
 
-    current_origin = LABEL_SIZE["height"] - (BARCODE_HEIGHT / 9) - LABEL_TEXT_SIZE["height"]
+    if text_bottom:
+        label.add_field_block(text_justification="C", width=label_width)
+        label.add_field_origin(x_pos=LABEL_PADDING, y_pos=current_origin, justification=2)
+        label.add_field_data(text_bottom)
+
+    current_origin = int(label_height - (BARCODE_HEIGHT * 2))
 
     if barcode:
-        # 28 = half
+        centered_left = 194 - int(((len(barcode) * 21) + 75) / 2)
+        barcode_zpl = Code128_Barcode(barcode, "N", BARCODE_HEIGHT, "Y", "N")
 
-        centered_left = 15 - (len(barcode))
-        label.origin(x=centered_left, y=current_origin)
-        label.barcode(barcode_type="2", code=barcode, height=BARCODE_HEIGHT, magnification=0.2)
-        label.endorigin()
+        label.add_field_block(width=label_width)
+        label.add_field_origin(x_pos=LABEL_PADDING + centered_left, y_pos=current_origin, justification=2)
+        label.add_barcode(barcode_zpl)
         current_origin = current_origin + (BARCODE_HEIGHT / 9) + 2
 
-    if text_bottom:
-        label.origin(x=0, y=current_origin)
-        label.write_text(
-            text_bottom,
-            char_height=LABEL_TEXT_SIZE["small_width"],
-            char_width=LABEL_TEXT_SIZE["small_height"],
-            line_width=LABEL_SIZE["width"],
-            justification="C",
-        )
-        label.endorigin()
-    label_text = label.dumpZPL()
-
-    mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    if socket.gethostname().lower() is not False:  # "chris-mbp":
-        mysocket.connect((config.PRINTER_HOST, config.PRINTER_PORT))  # connecting to host
-        for _ in range(quantity):
-            mysocket.send(bytes(label_text, "utf-8"))  # using bytes
-        mysocket.close()  # closing connection
+    printer = NetworkPrinter(config.PRINTER_HOST)
+    printer.print_zpl(label)
