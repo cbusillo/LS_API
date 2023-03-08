dc91ed25b18cc4e52d2f840c1560f7c0d0c99315
Chris Busillo <info@shinycomputers.com>
2023-03-08 01:06:35 -0500

    type hints

---
+++ typings/kivy/uix/button.pyi
@@ -0,0 +1,8 @@
+"""type stub for kivy.uix.button""" ""
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.label import Label
+
+class Button(ButtonBehavior, Label):
+    """Kivy button stub"""
+
+    text: str
