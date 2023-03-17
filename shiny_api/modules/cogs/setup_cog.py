2894df31e0a8b2ae70a93300a62eac1883bc3b78
Chris Busillo <info@shinycomputers.com>
2023-03-16 20:55:55 -0400

    send discord message on restart

--- shiny_api/modules/cogs/setup_cog.py
+++ shiny_api/modules/cogs/setup_cog.py
@@ -33,6 +33,7 @@ class SetupCog(commands.Cog):
             return_code = os.system("git diff origin/main --quiet")
             if return_code:
                 print("Restarting server!!!")
+                await context.channel.send("Restarting server!!!")
                 os.system("ssh 127.0.0.1 ~/launch_api.sh")
 
             await asyncio.sleep(2)
