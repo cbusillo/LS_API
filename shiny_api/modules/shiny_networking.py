646f194700e5f602e37c6a01b43ec370b692ee09
Chris Busillo <info@shinycomputers.com>
2023-03-30 18:55:05 -0400

    started helper function module

---
+++ shiny_api/modules/shiny_networking.py
@@ -0,0 +1,44 @@
+"""Module with useful netowrking functions"""
+import subprocess
+import paramiko
+
+
+def is_host_available(host: str) -> bool:
+    """Test if ping is successful"""
+    command = ["ping", "-c", "1", host]
+    response = subprocess.check_output(
+        command, stderr=subprocess.STDOUT, universal_newlines=True
+    )
+    if "1 packets received" in response:
+        return True
+    return False
+
+
+def scp_file_from_host(hostname: str, filename: str) -> bytes:
+    """Get file from remote.  Must have keys set up."""
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        ssh.connect(hostname)
+        # Read remote file contents as binary data
+        sftp = ssh.open_sftp()
+        remote_file = sftp.open(filename, mode="rb")
+        file_contents = remote_file.read()
+        remote_file.close()
+        sftp.close()
+
+        return file_contents
+
+    except paramiko.AuthenticationException:
+        print("Authentication failed. Please check your username and password.")
+    except paramiko.SSHException as ssh_error:
+        print(f"SSH error: {ssh_error}")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        ssh.close()
+
+
+if __name__ == "__main__":
+    print("Done.")
