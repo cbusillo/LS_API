dfaf70697ff6a242f08cc52180f84b91e7fc2d45
Chris Busillo <info@shinycomputers.com>
2023-03-31 08:25:11 -0400

    type hinting and docs

--- shiny_api/django_server/ls_functions/consumers.py
+++ shiny_api/django_server/ls_functions/consumers.py
@@ -1,9 +1,12 @@
+"""Class file for websocket consumer"""
 import json
 from channels.generic.websocket import WebsocketConsumer  # type: ignore
 from asgiref.sync import async_to_sync
 
 
 class LsFunctionsConsumer(WebsocketConsumer):
+    """Class to handle websocket connections"""
+
     def connect(self):
         self.accept()
         async_to_sync(self.channel_layer.group_add)("updates", self.channel_name)
@@ -16,7 +19,7 @@ class LsFunctionsConsumer(WebsocketConsumer):
         """Disconnect client from group"""
         async_to_sync(self.channel_layer.group_add("updates", self.channel_name))
 
-    def receive(self, text_data, _bytes_data):
+    def receive(self, text_data: str = "", _bytes_data: bytes = b""):
         """send received message back to client"""
         message = text_data
         self.send(text_data=message)
