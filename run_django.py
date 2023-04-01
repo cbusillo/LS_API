38b595dbd5ba5bcec6a4601891e25ff078f4b8d9
Chris Busillo <info@shinycomputers.com>
2023-04-01 09:43:56 -0400

    fix bug for remote launch

--- run_django.py
+++ run_django.py
@@ -2,6 +2,7 @@
 """File to run django server"""
 import logging
 import sys
+import subprocess
 from pathlib import Path
 import shiny_api.modules.django_server as django_server
 from shiny_api.modules.load_config import Config
@@ -12,20 +13,18 @@ def start_django_server():
     """Setup logging, get updated certificates, and start django server"""
     logging.basicConfig(level=logging.INFO)
 
-    if not is_host_available(Config.CERTIFICATE_SERVER_HOSTNAME):
-        django_server.main()
-        sys.exit()
-
-    for file_name in ["cert.pem", "privkey.pem", "fullchain.pem"]:
-        local_file_name = ".secret." + file_name
-        with open(Path.home() / local_file_name, "wb") as local_file:
-            remote_file = scp_file_from_host(
-                Config.CERTIFICATE_SERVER_HOSTNAME,
-                Config.CERTIFICATE_SERVER_FILE + file_name,
-            )
-            if remote_file:
-                local_file.write(remote_file)
-
+    if is_host_available(Config.CERTIFICATE_SERVER_HOSTNAME):
+        for file_name in ["cert.pem", "privkey.pem", "fullchain.pem"]:
+            local_file_name = ".secret." + file_name
+            with open(Path.home() / local_file_name, "wb") as local_file:
+                remote_file = scp_file_from_host(
+                    Config.CERTIFICATE_SERVER_HOSTNAME,
+                    Config.CERTIFICATE_SERVER_FILE + file_name,
+                )
+                if remote_file:
+                    local_file.write(remote_file)
+    print(subprocess.run(["pkill", "-f", "stunnel"], check=False))
+    print(subprocess.Popen("stunnel shiny_api/config/stunnel.ini", shell=True))
     django_server.main()
 
 
