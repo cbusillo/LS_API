a24133c10547434badbc26702a0d27f3468e4ec4
Chris Busillo <info@shinycomputers.com>
2023-04-02 16:39:53 -0400

    started sickw page and made FM lookup

--- shiny_api/django_server/label_printer/apps.py
+++ shiny_api/django_server/label_printer/apps.py
@@ -4,6 +4,5 @@ from django.apps import AppConfig  # type: ignore
 
 class LabelPrinterConfig(AppConfig):
     """Class to name Label Printer App"""
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'shiny_api.django_server.label_printer'
     label = 'label_printer'
