435bd61db7a7c5994dbe310d78d4cd1be299d8d1
Chris Busillo <info@shinycomputers.com>
2023-04-04 17:14:30 -0400

    fix changed path

--- shiny_api/django_server/api/views.py
+++ shiny_api/django_server/api/views.py
@@ -17,7 +17,7 @@ def workorder_label(request: WSGIRequest):
     workorder_id = int(request.GET.get("workorderID", 0))
     if workorder_id == 0:
         context["title"] = "No workorder ID"
-        return render(request, "error.django-html", context)
+        return render(request, "api/error.html", context)
     workorder = Workorder(workorder_id)
     for line in workorder.note.split("\n"):
         if line[0:2].lower() == "pw" or line[0:2].lower() == "pc":
@@ -31,7 +31,7 @@ def workorder_label(request: WSGIRequest):
     )
     if str(request.GET.get("manual")).lower() != "true":
         context["auto_close"] = "True"
-    return render(request, "close_window.django-html", context)
+    return render(request, "api/close_window.html", context)
 
 
 def ring_central_send_message(request: WSGIRequest):
@@ -46,19 +46,15 @@ def ring_central_send_message(request: WSGIRequest):
             mobile_number = phone.number
     if mobile_number is None:
         context["title"] = "No mobile number"
-        return render(request, "error.django-html", context)
+        return render(request, "api/error.html", context)
     message_number = int(request.GET.get("message", 0))
-    if (
-        workorder.total == 0 and request.GET.get("message") == "2"
-    ):  # if we send a message with price with $0 price
+    if workorder.total == 0 and request.GET.get("message") == "2":  # if we send a message with price with $0 price
         message_number += 1
     item_description = workorder.item_description
     for word in Config.STYLIZED_NAMES:
         if word.lower() in item_description.lower() and word not in item_description:
             index = item_description.lower().find(word.lower())
-            item_description = (
-                item_description[:index] + word + item_description[index + len(word) :]
-            )
+            item_description = item_description[:index] + word + item_description[index + len(word) :]
 
     message = Config.RESPONSE_MESSAGES[message_number]
     message = message.format(
