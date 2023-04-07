55ca045e5a7cbb2136101139d54db608ec4bd679
Chris Busillo <info@shinycomputers.com>
2023-04-07 18:48:44 -0400

    camera front end work

--- shiny_api/django_server/serial_camera/consumers.py
+++ shiny_api/django_server/serial_camera/consumers.py
@@ -1,10 +1,13 @@
 """Web socket consumer to receive and process frames from the client"""
 import base64
+from datetime import datetime, timedelta
+import json
 import time
 import threading
 import numpy
 import cv2
 import pytesseract
+import pytz
 from channels.generic.websocket import WebsocketConsumer
 
 
@@ -13,23 +16,31 @@ class CameraConsumer(WebsocketConsumer):
 
     desired_fps = 1
     last_processed_frame_time = None
+    confidence_level = 0
 
     def connect(self):
         """Run when new connection is established"""
         self.accept()
 
-    def receive(self, text_data=None, bytes_data=None):
+    def receive(self, text_data=None, _bytes_data=None):
         """Run when new frame is received"""
-        current_time = time.time()
+        if text_data is None:
+            return
+        frame_data = json.loads(text_data)
+        if self.is_frame_stale(frame_data.get("timestamp")):
+            return
 
-        if self.last_processed_frame_time is None or (current_time - self.last_processed_frame_time) >= 1 / self.desired_fps:
-            if threading.active_count() < 5:
-                ocr_thread = threading.Thread(target=self.process_frame, args=[text_data])
-                ocr_thread.start()
-            self.last_processed_frame_time = current_time
+        frame_image_data = frame_data.get("image")
+        if frame_image_data == b"":
+            return
+
+        if threading.active_count() < 10:
+            ocr_thread = threading.Thread(target=self.process_frame, args=[frame_image_data])
+            ocr_thread.start()
 
     def process_frame(self, text_data):
         """Process the frame"""
+        start_time = time.time()
         if text_data == "" or text_data == "Hello Server!":
             return
         cv_image = self.image_to_cv2(text_data)
@@ -39,23 +50,28 @@ class CameraConsumer(WebsocketConsumer):
 
         self.send_text(cleaned_text)
         self.send_image(cv_image)
+        finish_time = time.time()
+        time_taken = finish_time - start_time
+        print(f"{cleaned_text} {time_taken} seconds")
 
     def get_text_from_image(self, image) -> dict[str, str]:
         """Get dict from the image text"""
         image_data = pytesseract.image_to_data(
             image,
             output_type=pytesseract.Output.DICT,
-            config="--psm 11",  # -c tessedit_char_whitelist=' 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'",
+            config="--psm 11 -c tessedit_char_whitelist=': 0123456789ABCDEFGHIJKLMNPQRTUVWXYZ'",
         )
         return image_data
 
     def clean_serial_text(self, ocr_words):
         """Clean the serial text"""
         serial_text = {}
-        for conf, word in zip(ocr_words["conf"], ocr_words["text"]):
-            if conf < 40 or len(word) < 8:
+        for confidence, word in zip(ocr_words["conf"], ocr_words["text"]):
+            word = word.replace(":", "").strip()
+            if confidence < self.confidence_level or len(word) < 8:
                 continue
-            serial_text[word] = conf
+            serial_text[word] = confidence
+
         return serial_text
 
     def image_to_cv2(self, base64_data):
@@ -80,3 +96,12 @@ class CameraConsumer(WebsocketConsumer):
         _, buffer = cv2.imencode(".png", image)
         edge_image_base64 = base64.b64encode(buffer).decode("utf-8")
         self.send("image_stream: " + edge_image_base64)
+
+    def is_frame_stale(self, timestamp):
+        """Check if the frame is stale"""
+        timezone = pytz.timezone("America/New_York")
+        frame_datetime = datetime.fromisoformat(timestamp)
+        frame_datetime = frame_datetime.astimezone(timezone)
+        current_time = datetime.now().astimezone(timezone)
+        time_difference = current_time - frame_datetime
+        return time_difference > timedelta(seconds=1)
