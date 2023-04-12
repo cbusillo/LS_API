b70997bfdd88e52c6419f25f8a13ae96a5c44635
Chris Busillo <info@shinycomputers.com>
2023-04-12 09:20:06 -0400

    fixed init on migrations

--- migrations.sh
+++ migrations.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-if [ $1 = "init" ]
+if [ "$1" == "init" ]
 then
     echo "Initializing migrations"
     find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
     find . -path "*/migrations/*.pyc" -delete
     rm ~/.shiny/db.sqlite3
+    for directory in ./shiny_app/django_server/*/
+    do
+        if [[ $directory == *"__pycache__"* || $directory == *"templates"* || $directory == *"static" ]]
+        then
+            continue
+        fi
+        python shiny_app/modules/django_server.py makemigrations `basename $directory`
+    done
 
 fi
 
