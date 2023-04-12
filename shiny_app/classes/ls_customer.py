cf83a497f81ad0036f2d95699333ae308acf39a6
Chris Busillo <info@shinycomputers.com>
2023-04-12 18:22:40 -0400

    code cleanup.

--- shiny_app/classes/ls_customer.py
+++ shiny_app/classes/ls_customer.py
@@ -1,8 +1,9 @@
 """Class to import customer objects from LS API"""
+import logging
 from typing import Any
 from dataclasses import dataclass
 from datetime import datetime
-from shiny_app.classes.ls_client import Client
+from shiny_app.classes.ls_client import Client, string_to_datetime
 from shiny_app.modules.load_config import Config
 
 
@@ -10,74 +11,82 @@ from shiny_app.modules.load_config import Config
 class ContactAddress:
     """Contact Address"""
 
-    def __init__(self, obj: Any):
+    def __init__(self, address: dict[str, str]):
         """Load ContactAddress from dict"""
-        self.address1 = str(obj.get("address1"))
-        self.address2 = str(obj.get("address2"))
-        self.city = str(obj.get("city"))
-        self.state = str(obj.get("state"))
-        self.zip = str(obj.get("zip"))
-        self.country = str(obj.get("country"))
-        self.country_code = str(obj.get("countryCode"))
-        self.state_code = str(obj.get("stateCode"))
+        self.address1 = address.get("address1") or ""
+        self.address2 = address.get("address2") or ""
+        self.city = address.get("city") or ""
+        self.state = address.get("state") or ""
+        self.zip = address.get("zip") or ""
+        self.country = address.get("country") or ""
+        self.country_code = address.get("countryCode") or ""
+        self.state_code = address.get("stateCode") or ""
 
 
 @dataclass
 class ContactEmail:
     """Contact email from dict"""
 
-    def __init__(self, obj: dict["str", "str"]):
+    def __init__(self, email: dict[str, str]):
         """Contact email from dict"""
-        self.address = str(obj.get("address"))
-        self.use_type = str(obj.get("useType"))
+        self.address = email.get("address") or ""
+        self.use_type = email.get("useType") or ""
 
 
 @dataclass
 class ContactPhone:
     """Contact phone"""
 
-    def __init__(self, obj: Any):
+    def __init__(self, phone: dict[str, Any]):
         """Contact phone from dict"""
         # if isinstance(obj, dict):
-        self.number = str(obj.get("number"))
-        self.use_type = str(obj.get("useType"))
+        self.number = phone.get("number") or ""
+        self.use_type = phone.get("useType") or ""
 
 
 @dataclass
 class Emails:
     """Email class from LS"""
 
-    def __init__(self, obj: Any):
+    def __init__(self, emails: dict[str, Any]):
         """Emails from dict"""
-        if obj == "":
-            self.contact_email = []
-        elif isinstance(obj.get("ContactEmail"), list):
-            self.contact_email = [ContactEmail(y) for y in obj.get("ContactEmail")]
-        else:
-            self.contact_email = [ContactEmail(obj.get("ContactEmail"))]
+
+        if emails == "":
+            self.contact_emails = []
+            return
+        contact_emails = emails.get("ContactEmail")
+        if isinstance(contact_emails, list):
+            self.contact_emails = [ContactEmail(y) for y in contact_emails]
+        elif isinstance(contact_emails, dict):
+            self.contact_emails = [ContactEmail(contact_emails)]
 
 
 @dataclass
 class Phones:
     """Phones"""
 
-    def __init__(self, obj: Any):
+    def __init__(self, phones: dict[str, Any]):
         """Phones from dict"""
-        if obj == "":
-            self.contact_phone = []
-        elif isinstance(obj.get("ContactPhone"), list):
-            self.contact_phone = [ContactPhone(y) for y in obj.get("ContactPhone")]
-        else:
-            self.contact_phone = [ContactPhone(obj.get("ContactPhone"))]
+
+        if phones is "":
+            self.contact_phones = []
+            return
+        contact_phones = phones.get("ContactPhone")
+        if isinstance(contact_phones, list):
+            self.contact_phones = [ContactPhone(y) for y in contact_phones]
+        elif isinstance(contact_phones, dict):
+            self.contact_phones = [ContactPhone(contact_phones)]
 
 
 @dataclass
 class Addresses:
     """Address class from LS"""
 
-    def __init__(self, obj: Any):
+    def __init__(self, address: dict[str, str]):
         """Addresses from dict"""
-        self.contact_address = ContactAddress(obj.get("ContactAddress"))
+        contact_address = address.get("ContactAddress")
+        if isinstance(contact_address, dict):
+            self.contact_address = ContactAddress(contact_address)
 
 
 @dataclass
@@ -86,16 +95,16 @@ class Contact:
 
     def __init__(self, obj: Any):
         """Contact from LS"""
-        self.contact_id = str(obj.get("contactID"))
-        self.custom = str(obj.get("custom"))
-        self.no_email = str(obj.get("noEmail"))
-        self.no_phone = str(obj.get("noPhone"))
-        self.no_mail = str(obj.get("noMail"))
+        self.contact_id = obj.get("contactID") or ""
+        self.custom = obj.get("custom") or ""
+        self.no_email = obj.get("noEmail") or ""
+        self.no_phone = obj.get("noPhone") or ""
+        self.no_mail = obj.get("noMail") or ""
         self.addresses = Addresses(obj.get("Addresses"))
         self.phones = Phones(obj.get("Phones"))
         self.emails = Emails(obj.get("Emails"))
-        self.websites = str(obj.get("Websites"))
-        self.time_stamp = str(obj.get("timeStamp"))
+        self.websites = obj.get("Websites") or ""
+        self.time_stamp = obj.get("timeStamp") or ""
 
 
 @dataclass
@@ -104,27 +113,29 @@ class Customer:
 
     client = Client()
 
-    def __init__(self, customer_id: int = 0, ls_customer: Any = None):
+    def __init__(self, customer_id: int = 0, ls_customer: Any = None, client: Client | None = None):
         """Customer object from dict"""
         if ls_customer is None:
             if customer_id == 0:
                 raise ValueError("Customer ID or LS Customer object required")
             self.customer_id = customer_id
             ls_customer = self.client.get_customer_json(self.customer_id)
-        self.customer_id = ls_customer.get("customerID")
-
-        self.first_name = str(ls_customer.get("firstName")).strip()
-        self.last_name = str(ls_customer.get("lastName"))
-        self.title = str(ls_customer.get("title"))
-        self.company = str(ls_customer.get("company"))
-        self.create_time = str(ls_customer.get("createTime"))
-        self.time_stamp = str(ls_customer.get("timeStamp"))
+        self.customer_id = ls_customer.get("customerID") or 0
+        if self.customer_id == 0:
+            logging.error("No customer returned from LS (O customer_id)")
+            return
+        self.first_name = ls_customer.get("firstName").strip() or ""
+        self.last_name = ls_customer.get("lastName") or ""
+        self.title = ls_customer.get("title") or ""
+        self.company = ls_customer.get("company") or ""
+        self.create_time = string_to_datetime(ls_customer.get("createTime"))
+        self.time_stamp = string_to_datetime(ls_customer.get("timeStamp"))
         self.archived = ls_customer.get("archived").lower() == "true"
-        self.contact_id = str(ls_customer.get("contactID"))
-        self.credit_account_id = str(ls_customer.get("creditAccountID"))
-        self.customer_type_id = str(ls_customer.get("customerTypeID"))
-        self.discount_id = str(ls_customer.get("discountID"))
-        self.tax_category_id = str(ls_customer.get("taxCategoryID"))
+        self.contact_id = int(ls_customer.get("contactID"))
+        self.credit_account_id = int(ls_customer.get("creditAccountID"))
+        self.customer_type_id = int(ls_customer.get("customerTypeID"))
+        self.discount_id = int(ls_customer.get("discountID"))
+        self.tax_category_id = int(ls_customer.get("taxCategoryID"))
         self.contact = Contact(ls_customer.get("Contact"))
         self.is_modified = False
 
@@ -137,7 +148,7 @@ class Customer:
             return
 
         numbers = {}
-        for number in self.contact.phones.contact_phone:
+        for number in self.contact.phones.contact_phones:
             numbers[number.use_type] = number.number
 
         numbers["Mobile"] = (
