c2d3d932d8ee802a664f8eea96a78d144a7b9c9a
Chris Busillo <info@shinycomputers.com>
2023-04-14 08:36:45 -0400

    missing quotes

--- run_django.py
+++ run_django.py
@@ -24,7 +24,7 @@ def start_django_server():
                     local_file.write(remote_file)
     path = "/usr/local/bin:/opt/homebrew/bin/"
     print(subprocess.run(["pkill", "-f", "stunnel"], check=False))
-    print(subprocess.Popen(["/usr/bin/env", "-P", path, "python3.11", "shiny_app/modules/django_server.py migrate"], shell=False))
+    print(subprocess.Popen(["/usr/bin/env", "-P", path, "python3.11", "shiny_app/modules/django_server.py", "migrate"], shell=False))
 
     print(subprocess.Popen(["/usr/bin/env", "-P", path, "stunnel", "shiny_app/config/stunnel.ini"], shell=False))
     django_server.main()
