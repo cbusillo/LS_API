c4a0fd1e3774a61ed339c99724f7c73bbe66ad5a
Chris Busillo <info@shinycomputers.com>
2023-04-17 13:56:59 -0400

    add validation to create workorder

--- shiny_app/django_server/check_in/templates/check_in/home.js
+++ shiny_app/django_server/check_in/templates/check_in/home.js
@@ -25,6 +25,10 @@ createWorkorderButton.addEventListener('click', async function (event) {
     var submitData; // define the variable here
 
     if (!customerId) {
+        if (!validateCustomerInput()) {
+            return;
+        }
+
         submitData = {
             form: customerSearchForm.serialize(),
             csrfmiddlewaretoken: csrfToken
@@ -55,6 +59,7 @@ createWorkorderButton.addEventListener('click', async function (event) {
 
 
 customerOutput.change(function () {
+    removeInvalidFeedback();
     var customerId = $(this).val();
     if (customerId) {
         $.ajax({
@@ -84,16 +89,51 @@ customerOutput.change(function () {
 
 lastNameInput.on("input", function () {
     updateCustomerOutput();
+    removeInvalidFeedback($(this));
 });
 firstNameInput.on("input", function () {
     updateCustomerOutput();
+    removeInvalidFeedback($(this));
 });
 phoneNumberInput.on("input", function () {
     updateCustomerOutput();
+    removeInvalidFeedback($(this));
 });
 emailAddressInput.on("input", function () {
     updateCustomerOutput();
+    removeInvalidFeedback($(this));
 });
+
+function removeInvalidFeedback(field) {
+    if (customerOutput.val()) {
+        [firstNameInput, lastNameInput, phoneNumberInput, emailAddressInput].forEach((input) => {
+            input.removeClass('is-invalid');
+            input.next('.invalid-feedback').remove();
+        });
+    } else if (field) {
+        if (field.is(phoneNumberInput) && !isPhoneNumberValid(field.val().trim())) {
+            return;
+        }
+        if (field.is(emailAddressInput) && !isEmailAddressValid(field.val().trim())) {
+            return;
+        }
+
+        field.removeClass('is-invalid');
+        field.next('.invalid-feedback').remove();
+    }
+}
+
+
+
+function isPhoneNumberValid(phoneNumber) {
+    const digitCount = phoneNumber.replace(/\D+/g, '').length;
+    return digitCount >= 10;
+}
+
+function isEmailAddressValid(emailAddress) {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(emailAddress);
+}
 
 function updateCustomerOutput() {
 
@@ -125,6 +165,36 @@ function openWorkorderInSafari(workorderId) {
     window.open(workorderUrl, '_blank');
 }
 
+function validateCustomerInput() {
+    const firstName = firstNameInput.val().trim();
+    const lastName = lastNameInput.val().trim();
+    const phoneNumber = phoneNumberInput.val().trim();
+    const emailAddress = emailAddressInput.val().trim();
+
+    if (firstName === '') {
+        firstNameInput.addClass('is-invalid');
+        firstNameInput.after('<div class="invalid-feedback">First name is required.</div>');
+        valid = false;
+    }
+    if (lastName === '') {
+        lastNameInput.addClass('is-invalid');
+        lastNameInput.after('<div class="invalid-feedback">Last name is required.</div>');
+        valid = false;
+    }
+    if (!isPhoneNumberValid(phoneNumber)) {
+        phoneNumberInput.addClass('is-invalid');
+        phoneNumberInput.after('<div class="invalid-feedback">Phone number must be at least 10 digits long.</div>');
+        valid = false;
+    }
+
+    if (!isEmailAddressValid(emailAddress)) {
+        emailAddressInput.addClass('is-invalid');
+        emailAddressInput.after('<div class="invalid-feedback">Invalid email address format.</div>');
+        valid = false;
+    }
+
+    return valid;
+}
 
 customerOutput.val(customerOutput.find('option:first').val());
 customerOutput.trigger('change');
